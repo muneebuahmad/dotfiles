1656c3f4ad88ff22201f1a15cb8020ad5471a6b7
Muneeb Ahmad <muneebuahmad@gmail.com>
2020-05-29 00:46:30 -0500

    Initial commit

---
+++ aliases.zsh
@@ -0,0 +1,8 @@
+alias zshconfig="code ~/.zshrc"
+
+alias tolower="tr '[:upper:]' '[:lower:]'"
+alias toupper="tr '[:lower:]' '[:upper:]'"
+
+alias brewup="brew update; brew upgrade; brew cleanup; brew doctor"
+
+alias nodeupdate="nvm install node --reinstall-packages-from=node"
